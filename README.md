55bdaa0279dbc0dfd9b4d319abb6ecb495c84212
git9527 <398816613@qq.com>
2021-01-19 21:05:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#生成南通电视台直播列表
+
+基于https://img.ntjoy.com//templates/ntjoy/Public/new/httpInterfaceSign-min.js
+
+##示例文件如下
+
+```
+#EXTM3U
+#EXTINF:-1 tvg-id="120d425d" tvg-name="社教频道" tvg-logo="" group-title="南通电视台"
+http://nttv.wshls.homecdn.com/live/7cc7.m3u8?t=1611059935&k=090c4da70ad8bba147657c9079a756bf
+#EXTINF:-1 tvg-id="" tvg-name="公共频道" tvg-logo="" group-title="南通电视台"
+http://nttv.wshls.homecdn.com/live/7cc5.m3u8?t=1611059935&k=b24c162e9a1b8641b9ac968d3f16d18a
+#EXTINF:-1 tvg-id="016f902e" tvg-name="新闻综合" tvg-logo="" group-title="南通电视台"
+http://nttv.wshls.homecdn.com/live/7cc9.m3u8?t=1611059935&k=849a3a404d42edbaf8c2b7195cba295d
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+var nttv = require('./lib/nttv')
+const fs = require('fs')
+const file = '/tmp/nttv-m3u8/nttv.m3u8'
+
+function addLineToFile(content) {
+  const dir = '/tmp/nttv-m3u8'
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  fs.appendFileSync(file, content + '\n');
+}
+
+function insertChannel(channelId, tvgId, tvgName) {
+  nttv.getSignUrl(channelId, url => {
+    addLineToFile("#EXTINF:-1 tvg-id=\"" + tvgId + "\" tvg-name=\"" + tvgName + "\" tvg-logo=\"\" group-title=\"南通电视台\"")
+    addLineToFile(url)
+  })
+}
+
+fs.writeFileSync(file, '#EXTM3U\n')
+insertChannel("5e5d857d607dcc56016087facd760098", "016f902e", "新闻综合")
+insertChannel("5e5d857d607dcc56016087fa8f3e0095", "120d425d", "社教频道")
+insertChannel("5e5d857d607dcc56016087fa535a0092", "", "公共频道")
+
+
