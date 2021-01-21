7490f54012a8c8fc0cd962a72562b27ce5d60b71
git9527 <398816613@qq.com>
2021-01-22 00:17:45 +0800

    generate in seq

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function addLineToFile(content) {
   fs.appendFileSync(file, content + '\n');
 }
 
-function insertChannel(channelId, tvgId, tvgName) {
-  nttv.getSignUrl(channelId, url => {
-    addLineToFile("#EXTINF:-1 tvg-id=\"" + tvgId + "\" tvg-name=\"" + tvgName + "\" tvg-logo=\"\" group-title=\"南通电视台\"," + tvgName)
-    addLineToFile(url)
-  })
+async function insertChannel(channelId, tvgId, tvgName) {
+  const url = await nttv.getSignUrl(channelId)
+  addLineToFile("#EXTINF:-1 tvg-id=\"" + tvgId + "\" tvg-name=\"" + tvgName + "\" tvg-logo=\"\" group-title=\"南通电视台\"," + tvgName)
+  addLineToFile(url)
+  return new Promise(resolve => {resolve()})
 }
 
 const dir = '/tmp/nttv-m3u8'
@@ -19,8 +19,11 @@ if (!fs.existsSync(dir)){
 }
 
 fs.writeFileSync(file, '#EXTM3U\n')
+console.log('1')
+
 insertChannel("5e5d857d607dcc56016087facd760098", "061f902e", "新闻综合")
 insertChannel("5e5d857d607dcc56016087fa8f3e0095", "120d425d", "社教频道")
 insertChannel("5e5d857d607dcc56016087fa535a0092", "", "公共频道")
+
 
 
